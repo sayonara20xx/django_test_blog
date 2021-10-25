800fd3fc3ecde38f54fcf4259a74b886d8cf64cc
sayonara20xx <adyu.19@uni-dubna.ru>
2021-10-25 23:48:19 +0300

    modified AGAIN

--- edu_domoi/manage.py
+++ edu_domoi/manage.py
@@ -1,3 +1,26 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edu_domoi.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
+
 '''
     Здесь буду писать всё о структуре джанги в целом
 
@@ -57,26 +80,3 @@
     А этот файл (manage.py) представляет собой некий интерфейс, позволяющий контролировать
     и настраивать работу кода.
 '''
-
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
-import sys
-
-
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edu_domoi.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
